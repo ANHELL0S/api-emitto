80f5eea472ae96c528771fa68d794d8eebf30e92
Angelo <aangelogarcia2021@gmail.com>
2025-05-13 16:21:54 -0500

    Add custom message validation in SendEmailDto

--- src/modules/mail/dto/send-email.dto.ts
+++ src/modules/mail/dto/send-email.dto.ts
@@ -17,25 +17,40 @@ class AttachmentDto {
 }
 
 export class SendEmailDto {
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: 'El campo "from" debe ser una cadena de texto.' })
+  @IsNotEmpty({ message: 'El campo "from" no puede estar vacío.' })
   declare from: string
 
-  @IsString()
-  @IsNotEmpty()
+  @IsString({
+    message: 'El campo "subjectEmail" debe ser una cadena de texto.',
+  })
+  @IsNotEmpty({ message: 'El campo "subjectEmail" no puede estar vacío.' })
   declare subjectEmail: string
 
-  @IsArray()
-  @IsEmail({}, { each: true })
+  @IsArray({
+    message: 'El campo "sendTo" debe ser un arreglo de correos electrónicos.',
+  })
+  @IsEmail(
+    {},
+    {
+      each: true,
+      message:
+        'Cada dirección en "sendTo" debe ser un correo electrónico válido.',
+    },
+  )
   declare sendTo: string[]
 
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: 'El campo "message" debe ser una cadena de texto.' })
+  @IsNotEmpty({ message: 'El campo "message" no puede estar vacío.' })
   declare message: string
 
   @IsOptional()
-  @IsArray()
-  @ValidateNested({ each: true })
+  @IsArray({ message: 'El campo "attachments" debe ser un arreglo.' })
+  @ValidateNested({
+    each: true,
+    message:
+      'Cada archivo adjunto debe ser un objeto válido de tipo AttachmentDto.',
+  })
   @Type(() => AttachmentDto)
   declare attachments?: AttachmentDto[]
 }
